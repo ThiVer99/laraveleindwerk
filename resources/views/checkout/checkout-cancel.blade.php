3a40ab9edc6ac85551ccf8438e6e6ff61143dd30
ThiVer99 <vervaecke.thibo@gmail.com>
2023-06-01 15:52:13 +0200

    start product size's and color's

--- resources/views/checkout/checkout-cancel.blade.php
+++ resources/views/checkout/checkout-cancel.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
     <div class="min-vh-100 d-flex justify-content-center align-items-center">
-        <h1>ORDER CANCELT</h1>
+        <h1>The order has been cancelt</h1>
     </div>
 @endsection
